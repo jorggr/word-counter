059350d3f67118b45a10d64ff9fe9e1dc87550f9
JorgGR <j.org92@outlook.es>
2022-02-11 00:27:47 -0600

    word counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import { useState } from 'react'
 
 function App() {
+  // estado inicial, la funcion que actualizar el estado inicial
+  const [word, setWord] = useState(0)
+  // funcion que se ejecuta al escribir en nuestra entrada de texto
+  const launchCounter = (evt) => {
+    // obtenemos el valor
+    const text = evt.target.value
+    // separamos las palabras de los espacios
+    const separator = text.split(' ')
+    // eliminamos espacios en blanco creando un nuevo array con los elementos
+    // que cumplan x condicion, en este caso obtenemos los elementos diferentes a vacio
+    const filterWords = separator.filter((element) => element)
+    // verificamos la cantidad de elementos existentes
+    const counter = filterWords.length
+    console.log(evt.target.value)
+    // actualizamos el valor del estado inicial
+    setWord(counter)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="code">Usted ha escrito {word} palabras</h1>
+        <input type="text" className="App-input" onChange={launchCounter} />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
